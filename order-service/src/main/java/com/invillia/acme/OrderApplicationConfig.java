f8ac12aa7f8ac36906c096a39e47f5dffcc6dacf
Mario Eduardo Giolo <m.eduardo5@gmail.com>
2019-02-12 03:59:56 -0200

    Configs do Redis

---
+++ order-service/src/main/java/com/invillia/acme/OrderApplicationConfig.java
@@ -0,0 +1,28 @@
+package com.invillia.acme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class OrderApplicationConfig {
+
+	@Bean
+	public RedisConnectionFactory connectionFactory() {
+		return new LettuceConnectionFactory();
+	}
+
+	@Bean
+	public RedisTemplate<byte[], byte[]> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+		RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		return template;
+	}
+
+}
